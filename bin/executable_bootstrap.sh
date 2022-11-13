f646a76f63b3a1aeb8b741c596e0ad0a855688f9
Ben Draut <drautb@gmail.com>
2022-11-12 22:29:29 -0700

    Use home variable in expressions

--- bin/executable_bootstrap.sh
+++ bin/executable_bootstrap.sh
@@ -31,7 +31,7 @@ fi
 
 # Install oh-my-zsh in unattended mode
 set +x
-ZSH= sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+ZSH="" sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 set -x
 
 if [ -e ~/.zshrc.pre-oh-my-zsh ]; then
@@ -39,8 +39,8 @@ if [ -e ~/.zshrc.pre-oh-my-zsh ]; then
 fi
 
 # Install oh-my-zsh plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 
 # Install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
